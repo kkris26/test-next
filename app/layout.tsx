4e7d5f825e5a01038ca06bb4ee4ebd0bb40e32d8
kkris26 <krisnuartha09@gmail.com>
2024-11-10 06:11:56 +0800

    feat : Integrasi dengan CMS Prismic

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,26 @@ import type { Metadata } from "next";
 import Navbar from "@/components/main/Navbar";
 import "./globals.css";
 import Copyright from "@/components/main/Copyright";
+import { createClient } from "@/prismicio";
 
-export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This i My Portfolio",
-};
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient(); 
+  const page = await client.getSingle("settings");
+  
+  return{
+    title: page.data.site_title || "Fallback",
+    description: page.data.meta_description || "Fallback for Description",
+    icons: {
+      icon: page.data.site_icon.url || "",  // URL untuk site icon
+      shortcut: page.data.site_icon.url || "",  // URL untuk shortcut icon (favicon)
+    },
+    openGraph:{
+      images: [page.data.og_image.url || ""]
+    }
+  }
+}
+
 
 export default function RootLayout({
   children,
