4e7e9ae2850fb67b251e66685704f90b3412960f
kkris26 <krisnuartha09@gmail.com>
2024-11-10 11:10:40 +0800

    add image background

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 import { SliceZone } from "@prismicio/react";
-import Hero from "@/components/main/Hero";
+// import Hero from "@/components/main/Hero";
 
 export default async function Index() {
   const client = createClient(); 
   const home = await client.getSingle("homepage");
   return (
     <div>
-    <Hero/>
+    {/* <Hero/> */}
     <SliceZone slices={home.data.slices} components={components}/>
     </div>
   );
--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -15,7 +15,9 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="relative"
     >
+      <PrismicNextImage field={slice.primary.hero_image} />
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[2]">
 
 <PrismicRichText
